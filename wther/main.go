20fe5aaaef38a5534680448c043d889156c1935e
eplxy <steven.dvlam@gmail.com>
2026-05-28 13:29:38 -0400

    [wther] Allow api key configuration at build time via ldflags

--- wther/main.go
+++ wther/main.go
@@ -13,17 +13,25 @@ import (
 
 const url = "https://charm.sh/"
 
+// is set at build time using -ldflags, is overridden by .env in loadApiKey()
+var VersionDefaultApiKey string
+
+// overrides VersionDefaultApiKey
 func loadApiKey() error {
-	if err := godotenv.Load(); err != nil {
-		return err
+
+	_ = godotenv.Load()
+
+	if envKey, ok := os.LookupEnv("WEATHER_API_KEY"); ok && envKey != "" {
+		return nil
 	}
 
-	if _, ok := os.LookupEnv("WEATHER_API_KEY"); !ok {
-		return errors.New("WEATHER_API_KEY not set")
+	// if the key is not available in a .env, ensure that it was set at build time
+	if VersionDefaultApiKey != "" {
+		os.Setenv("WEATHER_API_KEY", VersionDefaultApiKey)
+		return nil
 	}
 
-	return nil
-
+	return errors.New("WEATHER_API_KEY not found in environment, .env, or binary build flags")
 }
 
 func main() {
